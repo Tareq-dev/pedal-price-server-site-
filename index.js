033f1f60ae76d553c6a6408cccedc96719f9e692
Tarequl Islam <tareque.dev@gmail.com>
2022-05-07 19:49:35 +0600

    heroku deploy

--- index.js
+++ index.js
@@ -16,23 +16,20 @@ app.get("/", (req, res) => {
 
 // Verify token
 
-function verifyJWT (req, res, next) {
+function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
     return res.status(401).send({ message: "Unauthorized Access" });
   }
   const token = authHeader.split(" ")[1];
-  // console.log(token);
   jwt.verify(token, process.env.ACCESS_KEY, (err, decoded) => {
     if (err) {
       res.status(403).send({ message: "Forbidded access" });
     }
-    // console.log("decoded", decoded);
     req.decoded = decoded;
     next();
   });
 }
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.clivt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -53,12 +50,15 @@ async function run() {
       res.send(products);
     });
 
-    app.get("/myproducts", async (req, res) => {
+    app.get("/myproducts", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
       const email = req.query.email;
-      const query = { email: email };
-      const cursor = productCollection.find(query);
-      const products = await cursor.toArray();
-      res.send(products);
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const cursor = productCollection.find(query);
+        const products = await cursor.toArray();
+        res.send(products);
+      }
     });
     // Get API by id
     app.get("/products/:id", async (req, res) => {
@@ -100,13 +100,13 @@ async function run() {
     });
 
     // auth JWT
-   app.post("/login", async (req, res) => {
-     const user = req.body;
-     const accessToken = jwt.sign(user, process.env.ACCESS_KEY, {
-       expiresIn: "1d",
-     });
-     res.send({accessToken});
-   });
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_KEY, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
   } finally {
   }
 }
